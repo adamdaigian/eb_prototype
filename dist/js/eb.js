9392b2f6d9c0e99f3327dd3c3e365d56023bc223
adam <adam@elasticbox.com>
2014-03-10 19:55:51 -0700

    added tracking html files and separate js file to be refactored

---
+++ dist/js/eb.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+
+  //mixpanel track
+  $('[track]').click(function () {
+    mixpanel.track($(this).attr("track"))  
+  });  
+
+  //use cases jquery  
+  $('#use-case-1').click(function () {
+    $('#use-case-select-1').addClass('active')
+    $('#use-case-carrot-1').addClass('active')
+    $('#use-case-lead-1').addClass('active')
+    $('#use-case-description-1').show()
+    $('#use-case-select-2').removeClass('active')
+    $('#use-case-carrot-2').removeClass('active')
+    $('#use-case-lead-2').removeClass('active')
+    $('#use-case-description-2').hide()
+    $('#use-case-select-3').removeClass('active')
+    $('#use-case-carrot-3').removeClass('active')
+    $('#use-case-lead-3').removeClass('active')
+    $('#use-case-description-3').hide()
+    $('#use-case-select-4').removeClass('active')
+    $('#use-case-carrot-4').removeClass('active')
+    $('#use-case-lead-4').removeClass('active')
+    $('#use-case-description-4').hide()
+  });
+  $('#use-case-2').click(function () {
+    $('#use-case-select-2').addClass('active')
+    $('#use-case-carrot-2').addClass('active')
+    $('#use-case-lead-2').addClass('active')
+    $('#use-case-description-2').show()
+    $('#use-case-select-1').removeClass('active')
+    $('#use-case-carrot-1').removeClass('active')
+    $('#use-case-lead-1').removeClass('active')
+    $('#use-case-description-1').hide()
+    $('#use-case-select-3').removeClass('active')
+    $('#use-case-carrot-3').removeClass('active')
+    $('#use-case-lead-3').removeClass('active')
+    $('#use-case-description-3').hide()
+    $('#use-case-select-4').removeClass('active')
+    $('#use-case-carrot-4').removeClass('active')
+    $('#use-case-lead-4').removeClass('active')
+    $('#use-case-description-4').hide()
+  });
+  $('#use-case-3').click(function () {
+    $('#use-case-select-3').addClass('active')
+    $('#use-case-carrot-3').addClass('active')
+    $('#use-case-lead-3').addClass('active')
+    $('#use-case-description-3').show()
+    $('#use-case-select-1').removeClass('active')
+    $('#use-case-carrot-1').removeClass('active')
+    $('#use-case-lead-1').removeClass('active')
+    $('#use-case-description-1').hide()
+    $('#use-case-select-2').removeClass('active')
+    $('#use-case-carrot-2').removeClass('active')
+    $('#use-case-lead-2').removeClass('active')
+    $('#use-case-description-2').hide()
+    $('#use-case-select-4').removeClass('active')
+    $('#use-case-carrot-4').removeClass('active')
+    $('#use-case-lead-4').removeClass('active')
+    $('#use-case-description-4').hide()
+  });
+  $('#use-case-4').click(function () {
+    $('#use-case-select-4').addClass('active')
+    $('#use-case-carrot-4').addClass('active')
+    $('#use-case-lead-4').addClass('active')
+    $('#use-case-description-4').show()
+    $('#use-case-select-1').removeClass('active')
+    $('#use-case-carrot-1').removeClass('active')
+    $('#use-case-lead-1').removeClass('active')
+    $('#use-case-description-1').hide()
+    $('#use-case-select-2').removeClass('active')
+    $('#use-case-carrot-2').removeClass('active')
+    $('#use-case-lead-2').removeClass('active')
+    $('#use-case-description-2').hide()
+    $('#use-case-select-3').removeClass('active')
+    $('#use-case-carrot-3').removeClass('active')
+    $('#use-case-lead-3').removeClass('active')
+    $('#use-case-description-3').hide()
+  });
+});
